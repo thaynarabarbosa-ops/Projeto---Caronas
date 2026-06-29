abf590e7a312e1ba30c2907b7a018ea0450888d5
thaynarabarbosa-ops <thaynara.barbosa@aluno.ifsp.edu.br>
2026-06-29 17:48:48 -0300

    CadastroServlet.java

--- CadastroServlet.java
+++ CadastroServlet.java
@@ -1,1 +1,46 @@
-public class CadastroServlet {}
+package java;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+
+@WebServlet("/CadastroServlet")
+public class CadastroServlet extends HttpServlet{
+
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response)
+            throws ServletException, IOException{
+
+        String nome = request.getParameter("nome");
+        String email = request.getParameter("email");
+        String senha = request.getParameter("senha");
+        String tipo = request.getParameter("tipo");
+
+        try{
+
+            Connection con = Conexao.conectar();
+
+            String sql = "INSERT INTO usuarios(nome,email,senha,tipo) VALUES(?,?,?,?)";
+
+            PreparedStatement ps = con.prepareStatement(sql);
+
+            ps.setString(1,nome);
+            ps.setString(2,email);
+            ps.setString(3,senha);
+            ps.setString(4,tipo);
+
+            ps.executeUpdate();
+
+            response.sendRedirect("html/login.html");
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+}
