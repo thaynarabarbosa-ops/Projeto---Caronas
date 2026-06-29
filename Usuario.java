04e22cab2202e302e0d16f789af822b616db9c5d
thaynarabarbosa-ops <thaynara.barbosa@aluno.ifsp.edu.br>
2026-06-29 17:46:51 -0300

    Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -1,1 +1,60 @@
-public class Usuario {}
+package java;
+
+public class Usuario {
+
+    private int id;
+    private String nome;
+    private String email;
+    private String senha;
+    private String tipo;
+
+    public Usuario(){}
+
+    public Usuario(String nome, String email, String senha, String tipo){
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.tipo = tipo;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getSenha(){
+        return senha;
+    }
+
+    public void setSenha(String senha){
+        this.senha = senha;
+    }
+
+    public String getTipo(){
+        return tipo;
+    }
+
+    public void setTipo(String tipo){
+        this.tipo = tipo;
+    }
+
+}
