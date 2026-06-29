0e6788874fd29d10c84b8573602cd429c0d3851d
thaynarabarbosa-ops <thaynara.barbosa@aluno.ifsp.edu.br>
2026-06-29 17:49:07 -0300

    Rota.java

--- Rota.java
+++ Rota.java
@@ -1,1 +1,60 @@
-public class Rota {}
+package java;
+
+public class Rota {
+
+    private int id;
+    private int motoristaId;
+    private String origem;
+    private String destino;
+    private String horario;
+    private int vagas;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getMotoristaId() {
+        return motoristaId;
+    }
+
+    public void setMotoristaId(int motoristaId) {
+        this.motoristaId = motoristaId;
+    }
+
+    public String getOrigem() {
+        return origem;
+    }
+
+    public void setOrigem(String origem) {
+        this.origem = origem;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    public int getVagas() {
+        return vagas;
+    }
+
+    public void setVagas(int vagas) {
+        this.vagas = vagas;
+    }
+
+}
