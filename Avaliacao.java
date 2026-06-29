aee32d60741b7bcae1208fc90ede5184173cd10d
thaynarabarbosa-ops <thaynara.barbosa@aluno.ifsp.edu.br>
2026-06-29 17:53:58 -0300

    Avaliacao.java

---
+++ Avaliacao.java
@@ -0,0 +1,24 @@
+package java;
+
+public class Avaliacao {
+
+    private int estrelas;
+    private String comentario;
+
+    public int getEstrelas() {
+        return estrelas;
+    }
+
+    public void setEstrelas(int estrelas) {
+        this.estrelas = estrelas;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+}
