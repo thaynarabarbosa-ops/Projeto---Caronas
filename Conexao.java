386d4ed1c4e876cc01890dddb1027780cfe8f832
thaynarabarbosa-ops <thaynara.barbosa@aluno.ifsp.edu.br>
2026-06-29 17:46:31 -0300

    Conexao.java

--- Conexao.java
+++ Conexao.java
@@ -1,1 +1,21 @@
-public class Conexao {}
+package java;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexao {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/caronas_ifsp";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection conectar() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
