522b6e6d37dff42fdfe58ff11f85599196a6f341
thaynarabarbosa-ops <thaynara.barbosa@aluno.ifsp.edu.br>
2026-06-29 17:51:45 -0300

    Solicitacao.java

---
+++ Solicitacao.java
@@ -0,0 +1,45 @@
+package java;
+
+public class Solicitacao {
+
+private int id;
+
+private int rotaId;
+
+private int passageiroId;
+
+private String status;
+
+public int getId() {
+return id;
+}
+
+public void setId(int id) {
+this.id=id;
+}
+
+public int getRotaId() {
+return rotaId;
+}
+
+public void setRotaId(int rotaId) {
+this.rotaId=rotaId;
+}
+
+public int getPassageiroId() {
+return passageiroId;
+}
+
+public void setPassageiroId(int passageiroId) {
+this.passageiroId=passageiroId;
+}
+
+public String getStatus() {
+return status;
+}
+
+public void setStatus(String status) {
+this.status=status;
+}
+
+}
